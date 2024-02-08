ea437d3c590a204abd0b6adc91d87170f1ce9d7e
Alberto <albertorg0087@gmail.com>
2024-02-08 17:08:28 +0000

    last commit

--- src/components/Map/FilterGroup/FilterOptions.jsx
+++ src/components/Map/FilterGroup/FilterOptions.jsx
@@ -25,8 +25,6 @@ function FilterOptions() {
     toggleGovFundsReceived(newState)
   }
 
-  
-
   return (
     <div className="flex flex-col gap-4">
       <div className="flex items-center space-x-2">
