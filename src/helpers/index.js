74efaf8d2010515e677c24b3fc90745fee850149
Alberto <albertorg0087@gmail.com>
2024-02-09 16:01:48 +0000

    last commit

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -8,15 +8,15 @@ import booleanPointInPolygon from "@turf/boolean-point-in-polygon";
  * @returns {boolean} - Verdadero si el elemento está dentro del polígono.
  */
 export const isWithinPolygon = (dataItem, searchPolygon) => {
-  if (!searchPolygon || searchPolygon.length === 0) return true;
+  if (!searchPolygon || searchPolygon.length === 0) return true
 
-  const hasValidCoordinates = dataItem.latitude != null && dataItem.longitude != null;
-  if (!hasValidCoordinates) return false;
+  const hasValidCoordinates = dataItem.latitude != null && dataItem.longitude != null
+  if (!hasValidCoordinates) return false
 
-  const itemCoords = [parseFloat(dataItem.longitude), parseFloat(dataItem.latitude)];
-  const itemPoint = point(itemCoords);
-  const polygonCoordinates = searchPolygon[0].map(coord => [coord.lng, coord.lat]);
-  polygonCoordinates.push(polygonCoordinates[0]); // Cierra el polígono.
+  const itemCoords = [parseFloat(dataItem.longitude), parseFloat(dataItem.latitude)]
+  const itemPoint = point(itemCoords)
+  const polygonCoordinates = searchPolygon[0].map(coord => [coord.lng, coord.lat])
+  polygonCoordinates.push(polygonCoordinates[0])
 
-  return booleanPointInPolygon(itemPoint, polygon([polygonCoordinates]));
-};
+  return booleanPointInPolygon(itemPoint, polygon([polygonCoordinates]))
+}
