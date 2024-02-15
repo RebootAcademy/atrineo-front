f32a4509dba227657315e928a3891e601063f2c8
Alberto <albertorg0087@gmail.com>
2024-02-15 11:39:20 +0000

    Draw Polygon Function Applied to Carol Components

--- src/components/Map/GnpLayer/GnpLayer.jsx
+++ src/components/Map/GnpLayer/GnpLayer.jsx
@@ -2,14 +2,16 @@ import { useContext } from "react"
 import { CollectionContext } from "../../../context/collection"
 import { Circle } from "react-leaflet"
 import { LayerContext } from "../../../context/layerContext"
+import { isWithinPolygon } from "../../../helpers"
 
 function GnpLayer() {
   const { collection } = useContext(CollectionContext)
-  const { gnp } = useContext(LayerContext)
+  const { gnp, searchPolygon } = useContext(LayerContext)
 
-  const companiesBySelectedGnp = collection[0]?.data?.filter((company) => {
-    return company.gnp <= gnp[0] 
-  })
+  const companiesBySelectedGnp = collection[0]?.data
+    .filter((company) => company.gnp <= gnp[0])
+    .filter((dataItem) => isWithinPolygon(dataItem, searchPolygon)
+  )
 
   const circles = companiesBySelectedGnp?.map(company => (
     <Circle
--- src/components/Map/LifeQualityLayer/LifeQualityLayer.jsx
+++ src/components/Map/LifeQualityLayer/LifeQualityLayer.jsx
@@ -2,19 +2,23 @@ import { useContext } from "react"
 import { CollectionContext } from "../../../context/collection"
 import { Circle } from "react-leaflet"
 import { LayerContext } from "../../../context/layerContext"
-import PopupComponent from "../../ui/PopupComponent/PopupComponent"
+import { isWithinPolygon } from "../../../helpers"
+// import PopupComponent from "../../ui/PopupComponent/PopupComponent"
 
 function LifeQualityLayer() {
   const { collection } = useContext(CollectionContext)
-  const { lifeQuality } = useContext(LayerContext)
+  const { lifeQuality, searchPolygon } = useContext(LayerContext)
 
-  const companiesBySelectedLifeQuality = collection[0]?.data?.filter((company) => {
-    return company.lifeQuality === lifeQuality
-  })
+  const companiesBySelectedLifeQuality = collection[0]?.data
+    .filter((company) => company.lifeQuality === lifeQuality)
+    .filter((dataItem) => isWithinPolygon(dataItem, searchPolygon)
+  )
 
   let pathOptions = { fillColor: "orange", stroke: false, fillOpacity: 0.3 }
+
   if (lifeQuality === 'medium') pathOptions = { fillColor: "yellow", stroke: false, fillOpacity: 0.3 }
   else if (lifeQuality === 'high') pathOptions = { fillColor: "green", stroke: false, fillOpacity: 0.3 }
+
   const circles = companiesBySelectedLifeQuality?.map(company => (
     <Circle
       key={company._id}
@@ -22,7 +26,7 @@ function LifeQualityLayer() {
       pathOptions={pathOptions}
       radius={4000}
     >
-        <PopupComponent name={company.name} />
+      {/* <PopupComponent name={company.name} /> */}
     </Circle>
   ))
 
