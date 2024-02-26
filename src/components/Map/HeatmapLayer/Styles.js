b0a67ac944c838f391a55fc5c18984da8fb6a850
Alber0087 <albertorg0087@gmail.com>
2024-02-26 17:36:04 +0000

    HeatmapLayer from map-test as example

---
+++ src/components/Map/HeatmapLayer/Styles.js
@@ -0,0 +1,40 @@
+export const selectedStyle = {
+  opacity: 0.7,
+  fillOpacity: 1,
+  fillColor: '#ebf5fb',
+}
+
+export const defaultStyle = {
+  opacity: 0.3,
+  fillOpacity: 0.8,
+  color: 'black',
+  fillColor: '#aed6f1',
+}
+
+export const below10 = {
+  opacity: 0.3,
+  fillOpacity: 0.8,
+  color: 'black',
+  fillColor: '#85c1e9',
+}
+
+export const between10and20 = {
+  opacity: 0.3,
+  fillOpacity: 0.8,
+  color: 'black',
+  fillColor: '#3498db',
+}
+
+export const between20and35 = {
+  opacity: 0.3,
+  fillOpacity: 0.8,
+  color: 'black',
+  fillColor: '#2874a6',
+}
+
+export const over35 = {
+  opacity: 0.3,
+  fillOpacity: 0.8,
+  color: 'black',
+  fillColor: '#1b4f72',
+}
