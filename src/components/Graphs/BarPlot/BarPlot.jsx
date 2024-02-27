991f4cf035b286c124c38338c04789bfa983ea71
JuanAntonioLeonOjeda <jalojr@hotmail.com>
2024-02-27 18:24:52 +0000

    BARPLOT: - Display graph by regions - Select vertical field

--- src/components/Graphs/BarPlot/BarPlot.jsx
+++ src/components/Graphs/BarPlot/BarPlot.jsx
@@ -1,20 +1,36 @@
 import PropTypes from 'prop-types'
-import { useMemo } from 'react'
+import { useMemo, useState } from 'react'
 import * as d3 from "d3" // we will need d3.js
 
-import BarItem from '../BarItem/BarItem'
-
-import { findMaxAndMinValues } from '../../../helpers'
-
-const MARGIN = { top: 30, right: 30, bottom: 30, left: 30 }
+const MARGIN = { top: 30, right: 30, bottom: 30, left: 80 }
 const BAR_PADDING = 0.3
 
-function Barplot ({ width, height, data, regions }) {
+function Barplot ({ width, height, data, regions, fields }) {
+
+  const [field, setField] = useState('patents')
 
   const boundsWidth = width - MARGIN.right - MARGIN.left
   const boundsHeight = height - MARGIN.top - MARGIN.bottom
+  
+  const handleChange = (e) => {
+    setField(e.target.value)
+  }
 
-  const [max] = findMaxAndMinValues(data, 'patents')
+  const summedData = useMemo(() => {
+    const sums = data.reduce((acc, cur) => {
+      const name = cur.locationId.division3?.name
+      if (!name) return acc
+      if (!acc[name]) {
+        acc[name] = 0
+      }
+      const [value] = cur.fields.filter(d => d.fieldName === field)
+      acc[name] += value.fieldValue
+      return acc
+    }, {})
+    return Object.entries(sums).map(([name, sum]) => ({ name, sum }))
+  }, [data, field])
+
+  const maxSum = useMemo(() => Math.max(...summedData.map(d => d.sum)), [summedData])
 
   const xScale = useMemo(() => {
     return d3
@@ -27,93 +43,48 @@ function Barplot ({ width, height, data, regions }) {
   const yScale = useMemo(() => {
     return d3
       .scaleLinear()
-      .domain([0, max])
+      .domain([0, maxSum])
       .range([boundsHeight, 0])
-  }, [max, boundsHeight])
-  
-  const allShapes = data.map((d, i) => {
-    const x = xScale(d.name)
-    if (x === undefined) {
-      return null
-    }
+  }, [maxSum, boundsHeight])
 
-    return (
-      <g key={i}>
-        <rect
-          x={xScale()}
-          y={yScale(d.name)}
-          width={xScale(d.value)}
-          height={yScale.bandwidth()}
-          opacity={0.7}
-          stroke="#9d174d"
-          fill="#9d174d"
-          fillOpacity={0.3}
-          strokeWidth={1}
-          rx={1}
-        />
-        <text
-          x={xScale()}
-          y={yScale + yScale.bandwidth() / 2}
-          textAnchor="end"
-          alignmentBaseline="central"
-          fontSize={12}
-          opacity={xScale(d.value) > 90 ? 1 : 0}
-        >
-          {d.value}
-        </text>
-        <text
-          x={xScale(0) + 7}
-          y={yScale + yScale.bandwidth() / 2}
-          textAnchor="start"
-          alignmentBaseline="central"
-          fontSize={12}
-        >
-          {d.name}
-        </text>
-      </g>
-    )
-  })
+  const xAxis = useMemo(() => d3.axisBottom(xScale), [xScale])
+  const yAxis = useMemo(() => d3.axisLeft(yScale), [yScale])
 
-  const grid = yScale
-    .ticks(5)
-    .slice(1)
-    .map((value, i) => (
-      <g key={i}>
-        <line
-          y1={yScale(value)}
-          y2={yScale(value)}
-          x1={0}
-          x2={boundsWidth}
-          stroke="#808080"
-          opacity={0.2}
-        />
-        <text
-          x={yScale(value)}
-          y={boundsWidth + 10}
-          textAnchor="middle"
-          alignmentBaseline="central"
-          fontSize={9}
-          stroke="#808080"
-          opacity={0.8}
-        >
-          {value}
-        </text>
-      </g>
-    ))
+  const bars = useMemo(() => summedData.map((d, i) => (
+    <rect
+      key={i}
+      x={xScale(d.name)}
+      y={yScale(d.sum)}
+      width={xScale.bandwidth()}
+      height={boundsHeight - yScale(d.sum)}
+      fill="#9d174d"
+    />
+  )), [xScale, yScale, summedData, boundsHeight])
 
   return (
-    <div>
+    <>
       <svg width={width} height={height}>
-        <g
-          width={boundsWidth}
-          height={boundsHeight}
-          transform={`translate(${[MARGIN.left, MARGIN.top].join(",")})`}
-        >
-          {grid}
-          {allShapes}
+        <g transform={`translate(${MARGIN.left},${MARGIN.top})`}>
+          {bars}
+          {/* Render X Axis */}
+          <g
+            transform={`translate(0,${boundsHeight})`}
+            ref={node => d3.select(node).call(xAxis)}
+            className="x-axis"
+          />
+          {/* Render Y Axis */}
+          <g
+            ref={node => d3.select(node).call(yAxis)}
+            className="y-axis"
+          />
         </g>
       </svg>
-    </div>
+      <select name="fields" id="field-select" onChange={handleChange}>
+        {
+          fields.map((f,i) => <option key={i} value={f.fieldName}>{f.fieldName}</option>)
+        }
+      </select>
+    </>
   )
 }
 
@@ -121,7 +92,8 @@ Barplot.propTypes = {
   width: PropTypes.number,
   height: PropTypes.number,
   data: PropTypes.array,
-  regions: PropTypes.array
+  regions: PropTypes.array,
+  fields: PropTypes.array
 }
 
 export default Barplot
--- src/pages/Statistics.jsx
+++ src/pages/Statistics.jsx
@@ -5,8 +5,7 @@ import BarPlot from '../components/Graphs/BarPlot/BarPlot'
 import { CollectionContext } from "../context/collectionContext"
 import { LayerContext } from "../context/layerContext"
 
-import { extractRegionNames } from "../helpers"
-
+import { extractRegionNames, extractNumericFields } from "../helpers"
 
 function Statistics () {
   const { collection } = useContext(CollectionContext)
@@ -14,10 +13,14 @@ function Statistics () {
 
   const regionNames = extractRegionNames(collection, mapDivision)
   const data = collection[0]?.data
+  let fields
+  if (data) {
+    fields = extractNumericFields(data[0].fields)
+  }
 
   return (
     <>
-      <BarPlot width={700} height={400} data={data} regions={regionNames}  />
+      <BarPlot width={700} height={400} data={data} regions={regionNames} fields={fields} />
     </>
   )
 }
