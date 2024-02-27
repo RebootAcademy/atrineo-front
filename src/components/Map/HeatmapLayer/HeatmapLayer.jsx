fe2af8f4174c611c07e9b0884cf93b9c890aece0
Alberto <albertorg0087@gmail.com>
2024-02-27 15:35:14 +0000

    last commit

--- src/components/Map/HeatmapLayer/HeatmapLayer.jsx
+++ src/components/Map/HeatmapLayer/HeatmapLayer.jsx
@@ -9,21 +9,19 @@ const HeatmapLayer = ({ data, fieldName }) => {
   const { mapDivision } = useContext(LayerContext)
   const mapData = useGeoJsonData(mapDivision)
 
-  console.log(fieldName)
-
   const setStyle = (feature) => {
     const currentGroupId = data.find(d => d.geojsonId === feature.properties.ID_3.toString())
-    console.log(currentGroupId)
-    const value = currentGroupId?.sums.find(sum => sum.fieldName === fieldName)?.total
-
-    if (value < 10000000) {
-      return below10
-    } else if (value >= 10000000 & value < 20000000) {
-      return between10and20
-    } else if (value >= 20000000 & value < 35000000) {
-      return between20and35
-    } else if (value > 35000000) {
-      return over35
+    if (currentGroupId) {
+      const value = currentGroupId?.sums.find(sum => sum.fieldName === fieldName)?.total
+      if (value < 10000000) {
+        return below10
+      } else if (value >= 10000000 & value < 20000000) {
+        return between10and20
+      } else if (value >= 20000000 & value < 35000000) {
+        return between20and35
+      } else if (value > 35000000) {
+        return over35
+      }
     } else {
       return defaultStyle
     }
