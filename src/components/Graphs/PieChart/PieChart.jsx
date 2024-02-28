841c5b76666957e4dbf0da39b5b0865424c41daa
caarolbrown <carolinamorenofdez@gmail.com>
2024-02-28 15:19:50 +0000

    trabajando juntos en PieChart obtenemos los datos que queremos mostrar, pero todavia no se renderiza la grafica

---
+++ src/components/Graphs/PieChart/PieChart.jsx
@@ -0,0 +1,62 @@
+import PropTypes from 'prop-types'
+import { useMemo } from "react"
+import * as d3 from "d3"
+
+const MARGIN = 30
+
+const colors = ["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56"]
+
+const PieChart = ({ width, height, data }) => {
+  const radius = Math.min(width, height) / 2 - MARGIN
+
+  const filteredData = data.flatMap(item =>
+    item.fields
+      .filter(field => field.fieldType === 'number' && !["latitude", "longitude", "districtId"].includes(field.fieldName))
+      .reduce((acc, field) => {
+        acc[field.fieldName] = field.fieldValue
+        return acc
+      }, {})
+  )
+  console.log(filteredData)
+
+  const pie = useMemo(() => {
+    const pieGenerator = d3.pie().value((d) => d.fields.fieldName)
+    return pieGenerator(data)
+  }, [data])
+
+  const arcs = useMemo(() => {
+    const arcPathGenerator = d3.arc()
+    return pie.map((p) =>
+      arcPathGenerator({
+        innerRadius: 0,
+        outerRadius: radius,
+        startAngle: p.startAngle,
+        endAngle: p.endAngle,
+      })
+    )
+  }, [radius, pie])
+
+  return (
+    <>
+      <svg width={width} height={height} style={{ display: "inline-block" }}>
+        <g transform={`translate(${width / 2}, ${height / 2})`}>
+          {arcs.map((arc, i) => {
+            return <path key={i} d={arc} fill={colors[i]} />
+          })}
+        </g>
+      </svg>
+      <div>
+        hola
+      </div>
+    </>
+  )
+}
+
+PieChart.propTypes = {
+  width: PropTypes.number,
+  height: PropTypes.number,
+  data: PropTypes.array,
+  fields: PropTypes.array
+}
+
+export default PieChart
--- src/pages/Statistics.jsx
+++ src/pages/Statistics.jsx
@@ -1,29 +1,31 @@
 import { useContext } from "react"
 
 import BarPlot from '../components/Graphs/BarPlot/BarPlot'
+import PieChart from "../components/Graphs/PieChart/PieChart"
 
 import { CollectionContext } from "../context/collectionContext"
 import { LayerContext } from "../context/layerContext"
 
-import { 
+import {
   extractRegionNames,
   extractNumericFields,
   extractStringOptions,
   extractBooleanOptions
 } from "../helpers"
 
-function Statistics () {
+function Statistics() {
   const { collection } = useContext(CollectionContext)
   const { mapDivision } = useContext(LayerContext)
 
-  
+
   let fields
   let data
   if (collection.length !== 0) {
     data = collection[0]?.data
     fields = extractNumericFields(data[0].fields)
   }
-  
+  console.log(data)
+
   const regionNames = extractRegionNames(collection, mapDivision)
   const stringOptions = extractStringOptions(data[0].fields)
   const booleanOptions = extractBooleanOptions(data[0].fields)
@@ -33,18 +35,28 @@ function Statistics () {
   return (
     <>
       {
-        collection.length === 0 ? 
-          'Loading...':
-          <BarPlot 
-            width={700} 
-            height={400} 
-            data={data} 
-            regions={regionNames} 
-            fields={fields} 
-            options={optionsArr} 
-            division={mapDivision} 
+        collection.length === 0 ?
+          'Loading...' :
+          <BarPlot
+            width={700}
+            height={400}
+            data={data}
+            regions={regionNames}
+            fields={fields}
+            options={optionsArr}
+            division={mapDivision}
           />
       }
+      {
+        collection.length === 0 ?
+          'Loading...' :
+          <PieChart
+            width={700}
+            height={400}
+            data={data}
+            fields={fields} />
+      }
+
     </>
   )
 }
