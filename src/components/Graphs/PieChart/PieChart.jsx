0defd23c94e5a3925298af00859e3a6900d48356
caarolbrown <carolinamorenofdez@gmail.com>
2024-02-29 11:33:55 +0000

    pieChart con información de los datos que muestra y el hover en cada parte del quesito

--- src/components/Graphs/PieChart/PieChart.jsx
+++ src/components/Graphs/PieChart/PieChart.jsx
@@ -1,14 +1,16 @@
 import PropTypes from 'prop-types'
-import { useMemo, useState } from "react"
+import { useMemo, useState, useRef } from "react"
 import * as d3 from "d3"
 
-const MARGIN = 30
+const MARGIN_X = 150
+const MARGIN_Y = 50
+const INFLEXION_PADDING = 20
 
 const colors = ["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56"]
 
 const PieChart = ({ width, height, data, regions, fields, division }) => {
   const [option, setOption] = useState(fields[0].fieldName)
-  console.log(option)
+  const ref = useRef(null)
 
   const handleChange = (e) => {
     setOption(e.target.value)
@@ -30,32 +32,94 @@ const PieChart = ({ width, height, data, regions, fields, division }) => {
 
   console.log(result)
 
-  const radius = Math.min(width, height) / 2 - MARGIN
+  const radius = Math.min(width - 2 * MARGIN_X, height - 2 * MARGIN_Y) / 2
 
   const pie = useMemo(() => {
     const pieGenerator = d3.pie().value((d) => d.value)
     return pieGenerator(result)
   }, [result])
 
-  const arcs = useMemo(() => {
-    const arcPathGenerator = d3.arc()
-    return pie.map((p) =>
-      arcPathGenerator({
-        innerRadius: 0,
-        outerRadius: radius,
-        startAngle: p.startAngle,
-        endAngle: p.endAngle,
-      })
+  const arcPathGenerator = d3.arc()
+
+  const shapes = pie.map((grp, i) => {
+    const sliceInfo = {
+      innerRadius: 0,
+      outerRadius: radius,
+      startAngle: grp.startAngle,
+      endAngle: grp.endAngle,
+    }
+    const centroid = arcPathGenerator.centroid(sliceInfo)
+    const slicePath = arcPathGenerator(sliceInfo)
+
+    const inflexionInfo = {
+      innerRadius: radius + INFLEXION_PADDING,
+      outerRadius: radius + INFLEXION_PADDING,
+      startAngle: grp.startAngle,
+      endAngle: grp.endAngle,
+    }
+    const inflexionPoint = arcPathGenerator.centroid(inflexionInfo)
+
+    const isRightLabel = inflexionPoint[0] > 0
+    const labelPosX = inflexionPoint[0] + 50 * (isRightLabel ? 1 : -1)
+    const textAnchor = isRightLabel ? "start" : "end"
+    const label = grp.data.name + " (" + grp.value + ")"
+
+
+    return (
+      <g
+        key={i}
+        className='transition-opacity duration-300 opacity-100 cursor-pointer hover:opacity-60'
+        onMouseEnter={() => {
+          if (ref.current) {
+            ref.current.classList.add('transition-opacity duration-300 opacity-20 cursor-pointer')
+          }
+        }}
+        onMouseLeave={() => {
+          if (ref.current) {
+            ref.current.classList.remove('transition-opacity duration-300 opacity-20 cursor-pointer')
+          }
+        }}
+      >
+        <path d={slicePath} fill={colors[i]} />
+        <circle cx={centroid[0]} cy={centroid[1]} r={2} />
+        <line
+          x1={centroid[0]}
+          y1={centroid[1]}
+          x2={inflexionPoint[0]}
+          y2={inflexionPoint[1]}
+          stroke={"black"}
+          fill={"black"}
+        />
+        <line
+          x1={inflexionPoint[0]}
+          y1={inflexionPoint[1]}
+          x2={labelPosX}
+          y2={inflexionPoint[1]}
+          stroke={"black"}
+          fill={"black"}
+        />
+        <text
+          x={labelPosX + (isRightLabel ? 2 : -2)}
+          y={inflexionPoint[1]}
+          textAnchor={textAnchor}
+          dominantBaseline="middle"
+          fontSize={14}
+        >
+          {label}
+        </text>
+      </g>
     )
-  }, [radius, pie])
+  })
 
   return (
     <>
       <svg width={width} height={height} style={{ display: "inline-block" }}>
-        <g transform={`translate(${width / 2}, ${height / 2})`}>
-          {arcs.map((arc, i) => {
-            return <path key={i} d={arc} fill={colors[i]} />
-          })}
+        <g
+          transform={`translate(${width / 2}, ${height / 2})`}
+          className='transition duration-300 opacity-100 cursor-pointer'
+          ref={ref}
+        >
+          {shapes}
         </g>
       </svg>
       <div>
