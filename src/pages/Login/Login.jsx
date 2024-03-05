fee9a477fc0aec1d2734edd2115ef278e8d95484
caarolbrown <carolinamorenofdez@gmail.com>
2024-03-05 10:14:55 +0000

    eye password done, falta refactorizar código para importar los svg de manera mas corta

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -8,8 +8,25 @@ function Login() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [errorMessage, setErrorMessage] = useState(false)
+  const [showPassword, setShowPassword] = useState(false)
 
   const navigate = useNavigate()
+
+  const openEye = <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M1.33398 7.99992C1.33398 7.99992 3.33398 3.33325 8.00065 3.33325C12.6673 3.33325 14.6673 7.99992 14.6673 7.99992C14.6673 7.99992 12.6673 12.6666 8.00065 12.6666C3.33398 12.6666 1.33398 7.99992 1.33398 7.99992Z" stroke="#09090B" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M8.00065 9.99992C9.10522 9.99992 10.0007 9.10449 10.0007 7.99992C10.0007 6.89535 9.10522 5.99992 8.00065 5.99992C6.89608 5.99992 6.00065 6.89535 6.00065 7.99992C6.00065 9.10449 6.89608 9.99992 8.00065 9.99992Z" stroke="#09090B" strokeLinecap="round" strokeLinejoin="round" />
+  </svg>
+
+  const closeEye = <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g clipPath="url(#clip0_1370_12806)">
+      <path d="M6.58732 6.58659C6.39082 6.76968 6.23321 6.99048 6.1239 7.23582C6.01459 7.48115 5.95581 7.74598 5.95107 8.01452C5.94633 8.28307 5.99573 8.54981 6.09632 8.79884C6.19691 9.04788 6.34663 9.2741 6.53655 9.46402C6.72646 9.65394 6.95269 9.80366 7.20172 9.90425C7.45076 10.0048 7.7175 10.0542 7.98604 10.0495C8.25459 10.0448 8.51942 9.98598 8.76475 9.87667C9.01008 9.76736 9.23088 9.60975 9.41398 9.41325M7.15398 3.38659C7.43486 3.35159 7.71761 3.33378 8.00065 3.33325C12.6673 3.33325 14.6673 7.99992 14.6673 7.99992C14.3693 8.638 13.9955 9.23787 13.554 9.78658M4.40732 4.40659C3.08148 5.30967 2.02057 6.55009 1.33398 7.99992C1.33398 7.99992 3.33398 12.6666 8.00065 12.6666C9.27792 12.67 10.5278 12.2967 11.594 11.5933M1.33398 1.33325L14.6673 14.6666" stroke="#09090B" strokeLinecap="round" strokeLinejoin="round" />
+    </g>
+    <defs>
+      <clipPath id="clip0_1370_12806">
+        <rect width="16" height="16" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
 
   const handleKeyPress = (event) => {
     console.log(event.key)
@@ -45,8 +62,8 @@ function Login() {
 
   return (
     <>
-      <div className='w-full h-full flex flex-col lg:flex-row justify-center items-center space-y-8 lg:space-y-8 md:space-x-24 lg:space-x-48'>
-        <div className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4'>
+      <div className='w-full h-full flex flex-col lg:flex-row md:flex-col justify-center items-center space-y- lg:space-y-8 md:space-x-24 lg:space-x-24'>
+        <div className='w-full sm:w-1/3 md:w-1/3 lg:w-1/4 xl:w-1/5'>
           <img
             src='./atrineo_icon-removebg-preview.png'
             alt='Atrineo icon'
@@ -86,17 +103,25 @@ function Login() {
                 Password:
               </label>
             </div>
-            <Input
-              type='password'
-              id='password'
-              name='password'
-              placeholder='Enter your password'
-              className='border p-2 rounded'
-              onKeyDown={handleKeyPress}
-              onChange={handlePassword}
-            />
-            <div className='flex justify-center mt-4'>
-              {errorMessage && <p style={{ fontSize: "12px" }}>Credentials are wrong</p>}
+            <div className='relative'>
+              <Input
+                type={showPassword ? 'text' : 'password'}
+                id='password'
+                name='password'
+                placeholder='Enter your password'
+                className='border p-2 rounded'
+                onKeyDown={handleKeyPress}
+                onChange={handlePassword}
+              />
+              <div
+                className='absolute top-3 right-4 cursor-pointer'
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? openEye : closeEye}
+              </div>
+              <div className='flex justify-center mt-4'>
+                {errorMessage && <p style={{ fontSize: "14px" }}>Credentials are wrong</p>}
+              </div>
             </div>
             <div className='flex justify-center'>
               <Button
@@ -105,16 +130,6 @@ function Login() {
               >
                 Login
               </Button>
-            </div>
-            <div className='flex justify-center mt-4'>
-              <input
-                type='checkbox'
-                id='forgotPasswordCheckbox'
-                className='mr-2'
-              />
-              <label htmlFor='forgotPasswordCheckbox'>
-                I forgot my password
-              </label>
             </div>
           </div>
         </div>
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -6,13 +6,13 @@ import Dataset from "../pages/Dataset"
 import Login from "../pages/Login/Login"
 import Layout from "../Layout/Layout"
 
-const isAuthenticated = () => {
-  if (!localStorage.getItem('token')) {
-    return redirect('/login')
-  } else {
-    return null
-  }
-}
+// const isAuthenticated = () => {
+//   if (!localStorage.getItem('token')) {
+//     return redirect('/login')
+//   } else {
+//     return null
+//   }
+// }
 
 export const router = createBrowserRouter([
   {
@@ -22,7 +22,7 @@ export const router = createBrowserRouter([
       {
         path: '/',
         element: <Login />,
-        loader: isAuthenticated()
+        //loader: isAuthenticated()
       },
       {
         path: '/map',
