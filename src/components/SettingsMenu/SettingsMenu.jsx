50d1e747333c406858f892b39d0e94aca8a8e9ef
caarolbrown <carolinamorenofdez@gmail.com>
2024-03-12 10:49:15 +0000

    modificacion del contenido de settings segun rol de usuario que hace login

--- src/components/SettingsMenu/SettingsMenu.jsx
+++ src/components/SettingsMenu/SettingsMenu.jsx
@@ -16,9 +16,12 @@ import {
   DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "@/components/ui/DropDown/DropdownMenu"
+import { useContext } from "react"
+import { UserContext } from "@/context/userContext"
 
 function SettingsMenu() {
   const navigate = useNavigate()
+  const { user } = useContext(UserContext)
   const onLogout = () => {
     localStorage.removeItem("token")
     localStorage.removeItem("id")
@@ -30,38 +33,45 @@ function SettingsMenu() {
   }
 
   return (
-    <div className="w-32 flex items-center">
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="ghost" >
-            {<SettingsIcon />}
-            <p className="ml-1">Settings</p>
+    <>
+      <div className="w-32 flex items-center">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" >
+              {<SettingsIcon />}
+              <p className="ml-1">Settings</p>
 
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="w-56">
-          <DropdownMenuGroup>
-            <DropdownMenuItem>
-              Admin Profile
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              User Profile
-            </DropdownMenuItem>
-            <DropdownMenuItem
-              onClick={onUpdateCsv}
-            >
-              Update CSV
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem
-              onClick={onLogout}
-            >
-              Log Out
-            </DropdownMenuItem>
-          </DropdownMenuGroup>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    </div>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-56">
+            <DropdownMenuGroup>
+              {user?.role === 'wizard' ? (
+                <>
+                  <DropdownMenuItem>
+                    Admin Profile
+                  </DropdownMenuItem>
+                  <DropdownMenuItem
+                    onClick={onUpdateCsv}
+                  >
+                    Update CSV
+                  </DropdownMenuItem>
+                </>
+              ) : (
+                <DropdownMenuItem>
+                  User Profile
+                </DropdownMenuItem>
+              )}
+              <DropdownMenuSeparator />
+              <DropdownMenuItem
+                onClick={onLogout}
+              >
+                Log Out
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </>
   )
 }
 
