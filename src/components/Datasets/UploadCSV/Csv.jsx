1a96bf23c0e1670ccfc7b2ce710b6fc460016d2e
caarolbrown <carolinamorenofdez@gmail.com>
2024-03-13 10:21:45 +0000

    commit despues de hacer pull de develop

--- src/components/Datasets/UploadCSV/Csv.jsx
+++ src/components/Datasets/UploadCSV/Csv.jsx
@@ -1,78 +1,49 @@
 import CSVReader from 'react-csv-reader'
-import { useRef, useState, useContext } from 'react'
-import { useQuery } from 'react-query'
-
 import { uploadCsv } from '../../../services/uploadData'
-
+import { useRef, useState } from 'react'
 import { Button } from '@/components/ui/Button/Button'
 
-import { CollectionContext } from '@/context/collectionContext'
-import { getPublicCollections } from '@/services/collectionService'
-import UpdateSpinner from '@/components/LoadingSpinner/UpdateSpinner'
-
 function Csv() {
-  const { setCollection } = useContext(CollectionContext)
-
   const [fileName, setFileName] = useState('')
-  const [dataBody,setDataBody] = useState({})
-  const [fetchData, setFetchData] = useState(false)
-  const [showUpdateSpinner, setShowUpdateSpinner] = useState(false)
-
   const fileInputRef = useRef(null)
 
   const handleButtonClick = () => {
     fileInputRef.current.click()
   }
 
-  useQuery('publicCollections', getPublicCollections, {
-    enabled: fetchData,
-    onSuccess: (data) => {
-      setCollection(data.result)
-      setFetchData(false)
-    }
-  })
-
   const handleCSVUpload = async (data, fileInfo) => {
     console.log("Data:", data)
     setFileName(fileInfo.name)
-    setDataBody(data)   
+    await uploadDataFile(data)
     console.log("File Information:", fileInfo)
   }
 
-  const uploadDataFile = async () => {
+  const uploadDataFile = async (data) => {
     try {
-      setShowUpdateSpinner(true)
-      const res = await uploadCsv(dataBody)
+      const res = await uploadCsv(data)
       if (res) {
-        setFetchData(true)
+        console.log(res)
       }
     } catch (error) {
       console.error(error)
-    } finally {
-      setShowUpdateSpinner(false)
     }
   }
 
   return (
-    <div className='flex justify-center'>
+    <div className='flex justify-between'>
       <div className='flex mr-2 border rounded-md'>
-        <button onClick={handleButtonClick} className='rounded-l-md bg-primary text-white px-4 text-sm'>Search File</button>
+        <button onClick={handleButtonClick} className='rounded-l-md bg-primary text-white px-4 text-sm font-semibold'>Search File</button>
         <div className='w-[600px] rounded-r-md flex items-center pl-3 text-sm'>
           {fileName ? fileName : "File not found"}
         </div>
       </div>
-      <Button onClick={uploadDataFile}>Update</Button>
+      <Button onClick={uploadDataFile} className='font-semibold'>Update</Button>
       <CSVReader
         ref={fileInputRef}
         onFileLoaded={handleCSVUpload}
         parserOptions={{ header: true, dynamicTyping: true, skipEmptyLines: true }}
         cssClass='hidden'
       />
-      {showUpdateSpinner && (
-        <div className='absolute inset-0 flex justify-center items-center bg-gray-500 bg-opacity-50 z-50'>
-          <UpdateSpinner/>
-        </div>
-      )}
     </div>
   )
 }
